092719cfc1f054853ef06d2ce273d153e18f95ae
brunokimura-dev <64974189+brunokimura-dev@users.noreply.github.com>
2024-12-26 14:43:37 -0300

    Update install.sh

--- Vbox/install.sh
+++ Vbox/install.sh
@@ -1,12 +1,16 @@
 sudo apt-get update
-sudo apt-get install -y gcc
+sudo apt-get install -y gcc python-pip unzip
 
 wget https://golang.org/dl/go1.12.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.12.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile && source ~/.profile
 
-git clone https://github.com/mininet/mininet.git
-./mininet/util/install.sh
+wget https://traffic.comics.unina.it/software/ITG/codice/D-ITG-2.8.1-r1023-src.zip
+cd D-ITG-2.8.1-r1023/src 
+make 
+sudo make install PREFIX=/usr/local 
+
+pip install mininet 
 
 echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding=1' | sudo tee -a /etc/sysctl.conf
